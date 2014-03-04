d4aa65ec8732f374e53f5fa3f7de1ce3025942d4
James Tease <jamestease@rockpooldigital.com>
2014-03-04 17:28:26 +0000

    borders

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('.box').click(function(e) {
         e.preventDefault();
+        var id = $(this).data('id');
         var after;
         if($(this).hasClass('last')) {
             after = $(this);
@@ -8,6 +9,14 @@ $(document).ready(function() {
             after = $(this).find(' ~ .last').eq(0);
         }
 
-        $('.template').insertAfter(after).slideDown().addClass('active');
+        if($('.template').hasClass('active') && $('.template').data('parent') === id) {
+            $('.template').slideUp().removeClass('active');
+        } else {
+            $('.template').insertAfter(after).slideDown().addClass('active').data('parent', id);
+            $('.template p').text($('.template p').text() + ' ' + id);
+            console.log($('.template').data('parent'));
+        }
+
+
     });
 });
